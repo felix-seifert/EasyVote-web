6ae19a5b9317f3d7d8c32b4c5f476a22aa645f4d
felix-seifert <mail@felix-seifert.com>
2019-05-21 01:12:34 +0800

    Minor Updates
    * Add favicon
    * Change texts
    * Remove SECUSO logo and change contact address

--- EasyVote/lang/de/76131/start.php
+++ EasyVote/lang/de/76131/start.php
@@ -1,7 +1,7 @@
 ﻿
 <div id="kkw">
     <h1>Wahl des Gemeinderats der Stadt Karlsruhe am 26. Mai 2019</h1>
-    <h2>Kumulieren und Panaschieren einfach gemacht durch die Simulation der Stimmabgabe mit EasyVote</h2>
+    <h2>Kumulieren und Panaschieren einfach gemacht - Simulation der Stimmabgabe mit EasyVote</h2>
     <br>
     <p>
         Diese Online-Wahlsimulation soll Ihnen dabei helfen, sich auf die Wahl des Gemeinderats der Stadt Karlsruhe
@@ -12,18 +12,28 @@
         werden kann.
     </p>
     <p>
-        Diese  Online-Wahlsimulation basiert auf dem EasyVote Konzept, welches in dem von der Deutschen
+        Diese Online-Wahlsimulation basiert auf dem EasyVote Konzept, welches in dem von der Deutschen
         Forschungsgemeinschaft (DFG) geförderten Forschungsprojekt "Verfassungskonforme Umsetzung von elektronischen
-        Wahlen" (VerKonWa) entstanden ist und die Grundlage der Dissertation von
+        Wahlen" (VerKonWa) von der Forschungsgruppe <a href="https://secuso.aifb.kit.edu/" target="_blank">SECUSO</a>
+        entwickelt wurde und die Grundlage der Dissertation von
         <a href="https://de.linkedin.com/in/dr-jurlind-budurushi-247a5b73" target="_blank" style="color: blue;">
-        Dr. Jurlind Budurushi</a> darstellt. Bei der Konzept-Erstellung standen die Kommunalwahlen in Hessen im Fokus
-        und Ziel war es, ein hybrides (elektronisch/Papier) Wahlsystem vorzuschlagen und zu evaluieren. Für die
-        Kommunalwahlen 2016 in Hessen wurde diese Online-Wahlsimulation aus dem EasyVote Konzept abgeleitet, um Wähler
-        beim Kumulieren und Panaschieren zu unterstützen und ihnen aufzuzeigen, welche Kreuze auf dem Stimmzettel
-        welche Auswirkungen haben. Die Regeln für die Kommunalwahlen in Hessen unterscheiden sich allerdings von denen
-        in Baden-Württemberg. Die Anpassung der Regeln und die damit verbundenen Änderungen hat
+            Dr. Jurlind Budurushi</a> an der <a href="http://tuprints.ulb.tu-darmstadt.de/5418/" target="_blank"
+                                                style="color: blue;">TU Darmstadt</a> darstellt. Bei der
+        Konzept-Erstellung standen die
+        Kommunalwahlen
+        in Hessen im Fokus und Ziel war es, ein hybrides (elektronisch/Papier) Wahlsystem vorzuschlagen und zu
+        evaluieren. Für die Kommunalwahlen 2016 in Hessen wurde diese Online-Wahlsimulation aus dem EasyVote Konzept
+        abgeleitet, um Wähler beim Kumulieren und Panaschieren zu unterstützen und ihnen aufzuzeigen, welche Kreuze auf
+        dem Stimmzettel welche Auswirkungen haben. Die Regeln für die Kommunalwahlen in Hessen unterscheiden sich
+        allerdings von denen in Baden-Württemberg. Die Anpassung der Regeln und die damit verbundenen Änderungen hat
         <a href="https://www.felix-seifert.com/" target="_blank" style="color: blue;">Felix Seifert</a>
-        (Wirtschaftsingenieursstudent am KIT) durchgeführt.
+        (Wirtschaftsingenieursstudent am <a href="https://secuso.aifb.kit.edu/" target="_blank"
+                                            style="color: blue;">KIT</a>) durchgeführt. Beim Hosting der Applikation
+        unterstützt
+        <a href="https://www.linkedin.com/in/jonas-pfrang-05684012a/" target="_blank" style="color: blue;">Jonas
+            Pfrang</a>
+        (Wirtschaftsingenieursstudent am <a href="https://secuso.aifb.kit.edu/" target="_blank"
+                                            style="color: blue;">KIT</a>) aktiv.
     </p>
     <p>
         Technische Hinweise:
@@ -33,7 +43,7 @@
                 ausschließlich die Navigation der Applikation.
             </li>
             <li>
-                Die Nutzung dieser Applikation ist zur Zeit nicht für Mobilgeräte geeignet.
+                Die Nutzung dieser Applikation ist zurzeit nicht für Mobilgeräte geeignet.
             </li>
             <li>
                 Ihre Auswahl an Kandidaten wird serverseitig nicht gespeichert.
@@ -43,13 +53,13 @@
                 weiterer Ansatz, die bei den Kommunalwahlen doch eher komplizierteren Wahlregeln zu verdeutlichen. Es
                 geht hierbei nicht um eine Stimmabgabe. Entsprechend haben wir nicht die Ressourcen aufgewendet, den
                 Server und den Dienst so abzusichern, wie das bei einer Stimmabgabe über das Internet der Fall sein
-                müsste. Wir speichern keinerlei Daten, die Sie deanonymisieren könnten.
+                müsste. Wir speichern keine Daten mit denen Rückschlüsse auf Ihre Auswahl möglich sind.
             </li>
         </ul>
     </p>
     <p>
         Bei Fragen oder Anregungen schreiben Sie uns eine E-Mail an:
-        <a href="mailto:kontakt@secuso.org" style="color: blue;">kontakt@secuso.org</a>
+        <a href="mailto:kontakt@secuso.org" style="color: blue;">wahlsimulation@felix-seifert.com</a>
     </p>
     <p>
         Weitere Informationen zu der Wahl finden Sie unter:
--- EasyVote/lang/de/info_right.php
+++ EasyVote/lang/de/info_right.php
@@ -3,8 +3,3 @@
         <img width="80%" src="bilder/EasyVote.png" alt="EasyVote">
     </a>
 </div>
-<div class="logo" style="padding-left:18%;padding-top:30px;">
-    <a href="https://www.secuso.org" target="blank">
-        <img width="80%" src="bilder/SECUSO.png" alt="SECUSO">
-    </a>
-</div>
